cbcd3819ad528b001d4bcf6b3f6761d4da184985
Maxim Mushynski <makmushinskiy@inbox.ru>
2023-04-12 16:32:16 +0300

    add template for events of villages

---
+++ hatyn_sisters/main/templates/main/js/app.js
@@ -0,0 +1,36 @@
+function myFunction(elem) {
+    elem = String(elem)
+    document.getElementById(elem).classList.toggle("show");
+}
+
+function filterFunction(drop) {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    drop = String(drop)
+    div = document.getElementById(drop);
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
+
+function filterFunction1(drop) {
+    var input, filter, ul, li, a, i, chapter;
+    chapter = document.querySelector('.myInput1');
+    filter = chapter.value.toUpperCase();
+    drop = String(drop)
+    div = document.getElementById(drop);
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
